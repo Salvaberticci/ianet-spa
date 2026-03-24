7ea587508dec9f666f83598729b4e9f31074f3cf
Salvatore Berticci <salvatoreberticci19@gmail.com>
2026-03-24 00:13:36 -0400

    fix: use NEXT_PUBLIC_API_BASE_URL for client-side fetches

--- ianet-spa/src/services/http.js
+++ ianet-spa/src/services/http.js
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:3001"
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3001"
 const DEFAULT_TIMEOUT = 10000
 
 export class HttpError extends Error {
